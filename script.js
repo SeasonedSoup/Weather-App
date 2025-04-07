98bf000418f80a4689f4f37f46ea46cd95705e07
SeasonedSoup <emmetteagoncillo1@gmail.com>
2025-04-07 12:22:15 +0800

    Show Data

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-const conditionSpan = document.querySelector('span')
+const spans = document.querySelectorAll('span')
 
+
+function getLocation(event) {
+    event.preventDefault();
+
+    const location = document.getElementById('location').value 
+    countryWeather(location);
+}
 async function countryWeather(country) {
-    const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}?key=XFXSMJ7YEFQ3GVL9LF3XRK4HK`)
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${country}?key=XFXSMJ7YEFQ3GVL9LF3XRK4HK`)
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        const convertedData = await convertJson(response)
 
-    const convertedData = await convertJson(weatherData)
+        console.log(convertedData);
+        const vals = getVals(convertedData);
 
-    console.log(convertedData);
-    conditionSpan.textContent = convertedData
+        for (let key in vals) {
+            const span = document.getElementById(key)
+
+            span.textContent = vals[key]
+        }
+    } catch (err) {
+        alert("Invalid Request", err)
+    }
 
 }
 
-function convertJson(weatherData) {
-    return weatherData.json().then((data) => {
-        return data.currentConditions.conditions;
+function convertJson(response) {
+    return  response.json().then((data) => {
+        return data
     })
 }
+
+function getVals(convertedData) {
+    obj = {
+        condition : convertedData.currentConditions.conditions,
+        address : convertedData.resolvedAddress,
+        description : convertedData.description,
+        temp : convertedData.currentConditions.temp
+    }
+    return obj;
+} 
